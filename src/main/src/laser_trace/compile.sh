9f15e9634f227fd0f6e5a35ab3a32d747a9a1479
Jonathan Caines <jec845@student.bham.ac.uk>
2021-12-02 16:46:07 +0000

    yes

--- src/main/src/laser_trace/compile.sh
+++ src/main/src/laser_trace/compile.sh
@@ -4,4 +4,4 @@
 
 g++ laser_trace.cpp -fPIC -O3  -msse4 -shared -I/usr/include/python3.8 -lboost_python38 -lpython3.8 -o laser_trace.so
 
-mv laser_trace.so ../pf_localisation/
+mv laser_trace.so ../main/
